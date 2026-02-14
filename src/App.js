b3a62200d3f2c4a8bd1f9eb1b31540ca503efe95
jithendrababug <jithendrababug@gmail.com>
2026-02-14 16:45:28 +0530

    Update the route to Home Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
-  return <Dashboard />;
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
+  );
 }
-
-export default App;
